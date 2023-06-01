31ecd5eb44d15aeb448a91d7eb8850c6afb2e1b0
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-01 19:57:54 +0200

    Correctivos estéticos

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -13,7 +13,7 @@ export class BooksComponent {
     new Books('Las montañas de la locura', 'Comedia', 'Pepe Villuela', 19.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/at-mountains-of-madness-lovecraft-9781447468806-cover-288x445.jpg', 2, 1),
     new Books('Los mitos de Cthulhu', 'Horror', 'H.P. Lovecraft', 16.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/tales-in-cthulhu-mythos-lovecraft-9781447468912-cover-288x445.jpg', 5, 2),
     new Books('El llamado de Cthulhu', 'Horror', 'H.P. Lovecraft', 15.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/call-of-cthulhu-lovecraft-9781447418320-cover-288x445.jpg', 8, 4),
-    new Books('El caso de Charles Dexter Ward', 'Horror', 'H.P. Lovecraft', 11.99, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1650342685i/30308694.jpg', 6, 4),
+    new Books('El caso de Charles Dexter Ward', 'Horror', 'H.P. Lovecraft', 11.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/outsider-lovecraft-9781528717175-cover-288x445.jpg', 6, 4),
 
   ]
 
