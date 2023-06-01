188584ec12ed12999344580dd6dae72275b66f16
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-01 18:47:21 +0200

    tarjetas

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -9,14 +9,14 @@ import { Books } from 'src/app/models/books';
 
 export class BooksComponent {
   books: Books[] = [
-    new Books('El horror de Dunwich', 'Horror', 'H.P. Lovecraft', 12.99, 'https://example.com/lovecraft1.jpg', 3, 2),
-    new Books('Las montañas de la locura', 'Comedia', 'Pepe Villuela', 19.99, 'photo2.jpg', 2,1),
-    new Books('Los mitos de Cthulhu', 'Horror', 'H.P. Lovecraft', 16.99, 'https://example.com/lovecraft3.jpg', 5, 2),
-   // new Books('El llamado de Cthulhu', 'Horror', 'H.P. Lovecraft', 15.99, 'https://example.com/lovecraft6.jpg', 8, 4),
-   // new Books('El caso de Charles Dexter Ward', 'Horror', 'H.P. Lovecraft', 11.99, 'https://example.com/lovecraft4.jpg', 6, 4),
+    new Books('El horror de Dunwich', 'Horror', 'H.P. Lovecraft', 12.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/dunwich-horror-lovecraft-9781447468554-cover-288x445.jpg', 3, 2),
+    new Books('Las montañas de la locura', 'Comedia', 'Pepe Villuela', 19.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/at-mountains-of-madness-lovecraft-9781447468806-cover-288x445.jpg', 2, 1),
+    new Books('Los mitos de Cthulhu', 'Horror', 'H.P. Lovecraft', 16.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/tales-in-cthulhu-mythos-lovecraft-9781447468912-cover-288x445.jpg', 5, 2),
+    new Books('El llamado de Cthulhu', 'Horror', 'H.P. Lovecraft', 15.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/call-of-cthulhu-lovecraft-9781447418320-cover-288x445.jpg', 8, 4),
+    new Books('The Complete Cthulhu Mythos Tales', 'Horror', 'H.P. Lovecraft', 11.99, 'https://www.readandcobooks.co.uk/wp-content/uploads/lovecrafts-scariest-stories-lovecraft-9781528717281-cover-288x445.jpg', 6, 4),
 
   ]
-  
+
 
   removeCard(book: Books) {
     const index = this.books.indexOf(book);
@@ -25,11 +25,10 @@ export class BooksComponent {
     }
   }
 
-  CargarBook(_book: Books)
-  {
+  CargarBook(_book: Books) {
     this.books.push(_book)
-    console.log("Libro añadido correctamente - "+ _book.title )
+    console.log("Libro añadido correctamente - " + _book.title)
   }
 
-  }
+}
 
